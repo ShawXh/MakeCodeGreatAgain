2af689c27a6aa2de748c8f0b0598c1e640304bcf
xh <taxuexh@sjtu.edu.cn>
2020-10-09 12:38:08 +0800

    39

--- leetcode/39.cpp
+++ leetcode/39.cpp
@@ -1,27 +1,34 @@
 #include<vector>
 #include<unordered_map>
 #include<iostream>
+#include <algorithm>
 using namespace std;
 
 class Solution {
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {  
-        vector<vector<int> > ss;
-        vector<int> s;
-        vector<int> idx;
-        s.push_back(candidates[0]);
-        idx.push_back(0);
+        vector<vector<int> > s;
+        vector<int> t;
+        sort(candidates.begin(), candidates.end());
+        func(s, t, candidates, 0, target);
+        return s;
+    }
 
-        int curi;
-        int sum = candidates[0];
-        while (!s.empty()) {
-            curi = idx[idx.size()-1];
-            sum += 
-
-        
+    void func(vector<vector<int> > & s, vector<int> & t, vector<int> & candidates, int i, int target) {
+        if (i+1>candidates.size()) return;
+        if (target == candidates[i]) {
+            t.push_back(candidates[i]);
+            s.push_back(t);
+            t.pop_back();
+            return;
         }
-
-
+        if (target < candidates[i]) {
+            return;
+        }
+        t.push_back(candidates[i]);
+        func(s, t, candidates, i, target-candidates[i]);
+        t.pop_back();
+        func(s, t, candidates, i + 1, target);
     }
 };
 
